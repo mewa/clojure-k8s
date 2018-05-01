3c391f541ac801f4471119f36676bb7335d5b56c
mewa <marcin.k.chmiel@gmail.com>
2018-05-02 01:33:22 +0200

    Add get job info function

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.4.474"]
                  [clj-http "2.0.0"]
                  [ring "1.6.3"]
                  [cheshire "5.5.0"]]
--- src/k8s/core.clj
+++ src/k8s/core.clj
@@ -1,9 +1,11 @@
 (ns k8s.core
   (:gen-class)
-  (:require [kubernetes.core :as core]
+  (:require [clojure.core.async :as async :refer [go chan close! <! >! <!! >!!]]
             [clojure.string :as s]
+            [kubernetes.core :as core]
             [kubernetes.api.core :as k8score]
             [kubernetes.api.batch-v- :as k8sbatch]
+            [kubernetes.api.core-v- :as k8scorev]
             [cheshire.core :refer :all]
             [ring.adapter.jetty :as jetty]))
 
@@ -14,7 +16,13 @@
 
 (def kube-config {:base-url "http://localhost:8001"})
 
-(defn run-job [cmd]
+(defn run-k8s [f & args]
+  (core/with-api-context kube-config
+    (try (apply f args)
+         (catch Exception e {:error (or (ex-data e) e)}))))
+
+
+(defn new-job [cmd]
   (k8sbatch/create-batch-v1-namespaced-job
    "default"
    {:metadata {:name (str "k8s-job-" (serial))}
@@ -23,10 +31,43 @@
                                            :command ["sh" "-c" cmd]}]
                              :restartPolicy "Never"}}}}))
 
-(defn run [cmd]
-  (core/with-api-context kube-config
-    (try (run-job cmd)
-         (catch Exception e {:error (or (ex-data e) e)}))))
+(defn job-pods [id]
+  (let [pods (chan)]
+    (go
+      (doseq
+          [pod
+           (map (fn [v] {:name (get-in v [:metadata :name])
+                         :status (get-in v [:status :phase])})
+                (:items (k8scorev/list-core-v1-namespaced-pod
+                         "default"
+                         {:label-selector (str "job-name=" id)})))]
+        (>! pods pod))
+      (close! pods))
+    pods))
+
+(defn pod-logs [pods]
+  (let [logs (chan)]
+    (go
+      (loop [podinfo (<! pods)]
+        (when podinfo
+          (let [pod (:name podinfo)
+                status (:status podinfo)]
+            (>! logs
+                {:pod pod
+                 :status status
+                 :log (k8scorev/read-core-v1-namespaced-pod-log pod "default")})
+            (recur (<! pods)))))
+      (close! logs))
+    logs))
+
+(defn get-job [id]
+  (let [pods (job-pods id)
+        logs (pod-logs pods)]
+    (loop [log (<!! logs)
+           acc nil]
+      (if log
+        (recur (<!! logs) (cons log acc))
+        acc))))
 
 (defn run-handler [request]
   (let [resp ((comp run slurp :body) request)]
